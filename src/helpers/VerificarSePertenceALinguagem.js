0f6dc1e333746245cdbbf20cc2a30e93efc68757
will <william.lucas.mehler@gmail.com>
2018-06-30 16:15:13 -0300

    Exibir as transições ao testar os símbolos
    Signed-off-by: will <william.lucas.mehler@gmail.com>

--- src/helpers/VerificarSePertenceALinguagem.js
+++ src/helpers/VerificarSePertenceALinguagem.js
@@ -3,58 +3,82 @@ import { Card } from 'antd';
 
 import { agruparTransicoesPorEstado } from './index';
 
-const simbolosASeremTestados = ['0', '0', '1'];
+// @TODO Trocar para variávei recebida pelo formulário
+const SIMBOLOS_A_SEREM_TESTADOS = ['0', '0', '1'];
 
 export default class VerificadorAfnAfnd extends Component {
 
-  analisar(formPayload) {
+  verificarSePertenceALinguagem(formPayload) {
 		if (formPayload.transicoes) {
+      const testeTransicao = this.testarSimbolos(formPayload);
+      return testeTransicao.pertenceALinguagem;
+    }
+  };
 
-      let estadoAtual = formPayload.estadoInicial;
-      const transicoes = [];
+  exibirAceitaOuRejeita(formPayload) {
+    const pertenceALinguagem = this.verificarSePertenceALinguagem(formPayload);
+    return pertenceALinguagem ? 'Aceita' : 'Rejeita'
+  }
 
-      const transicoesPorEstado = agruparTransicoesPorEstado(formPayload.transicoes);
-      simbolosASeremTestados.forEach((simbolo) => {
+  exibirTransicoes(formPayload) {
+    if (formPayload.transicoes) {
+      const transicoesGeradas = this.testarSimbolos(formPayload).transicoes;
+      const transicoesSimplificadas = this.simplificarTransicoes(transicoesGeradas);
+      return transicoesSimplificadas.join(' -> ');
+    }
+  }
 
-        const possiveisCaminhos = transicoesPorEstado[estadoAtual]
+  testarSimbolos(formPayload) {
+    let estadoAtual = formPayload.estadoInicial;
+    const transicoes = [];
+    const transicoesPorEstado = agruparTransicoesPorEstado(formPayload.transicoes);
 
-        let encontrouCaminho = false;
-        possiveisCaminhos.forEach((caminho)=> {
-          if (caminho.simbolo === simbolo) {
-            transicoes.push(caminho);
-            encontrouCaminho = true;
-            estadoAtual = caminho.resultado;
-          }
-        });
+    SIMBOLOS_A_SEREM_TESTADOS.forEach((simbolo) => {
 
-        if (!encontrouCaminho) {
-          return {
-            transicoes,
-            pertenceALinguagem: false
-          }
+      const possiveisCaminhos = transicoesPorEstado[estadoAtual];
+
+      let encontrouCaminho = false;
+      possiveisCaminhos.forEach((caminho)=> {
+        if (caminho.simbolo === simbolo) {
+          transicoes.push(caminho);
+          encontrouCaminho = true;
+          estadoAtual = caminho.resultado;
         }
       });
 
-      console.log({
-        transicoes,
-        pertenceALinguagem: true
-      });
+      if (!encontrouCaminho) {
+        console.log('não encontrou o caminho');
+        return {
+          transicoes,
+          pertenceALinguagem: false
+        };
+      }
+    });
 
-      this.simplificarTransicoes(transicoes);
-		}
-  };
+    console.log({
+      transicoes,
+      pertenceALinguagem: true
+    });
+
+    return {
+      transicoes,
+      pertenceALinguagem: true
+    };
+  }
 
   simplificarTransicoes(transicoes) {
     const transicoesSimplificadas = [];
     transicoes.forEach((transicao, index) => {
       if (index === 0) {
         transicoesSimplificadas.push(transicao.estado);
+        transicoesSimplificadas.push(transicao.resultado);
       } else {
         transicoesSimplificadas.push(transicao.resultado);
       }
     });
 
     console.log('transicoesSimplificadas', transicoesSimplificadas);
+    return transicoesSimplificadas;
   }
 
 	render() {
@@ -64,7 +88,8 @@ export default class VerificadorAfnAfnd extends Component {
 		return (
 			<Fragment>
 				<Card style={cardStyle}>
-					<h1>{this.analisar(this.props.formPayload)}</h1>
+					<h1>{this.exibirTransicoes(this.props.formPayload)}</h1>
+          <h1>{this.exibirAceitaOuRejeita(this.props.formPayload)}</h1>
 				</Card>
 			</Fragment>
 		);
